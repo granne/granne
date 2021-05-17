963363c3a5c9357d119accfc5f8e8238c234cd5c
Ivan Menshikh <menshikh.iv@gmail.com>
2021-05-17 21:38:42 +0500

    get rid nightly, bump cpython version

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools_rust import Binding, RustExtension
 
 import os
 
-os.environ['RUSTUP_TOOLCHAIN'] = 'nightly'
 
 setup(name='granne',
-      version='0.5.0',
+      version='0.5.2',
       rust_extensions=[RustExtension('granne',
                                      'py/Cargo.toml', binding=Binding.RustCPython)],
       zip_safe=False,
